dc8ea8708e9e6b4a4c6a42e341cb56f078d885c1
OnofreB22 <oabenjumev@gmail.com>
2022-05-19 03:39:55 -0500

    .

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <string>
 #include <iostream>
+#include <algorithm>
+#include <list>
 #include "persona.h"
 #include "bici.h"
 
 using namespace std;
 
+void addList(list<Bici> bicicletas, Bici bici);
+void viewList(list<Bici> bicicletas);
+void countList(list<Bici> bicicletas);
+
 int main(){
+    std::list<Bici> bicicletas;
+
     Persona *objPersona = new Persona("Onofre Benjumea", "3137601600", "oabenjumev@gmail.com");
     Bici *objBici = new Bici();
     objBici->verObjeto();
@@ -14,4 +22,18 @@ int main(){
     objBici->verObjeto();
 
     return 0;
+}
+
+void addList(list<Bici> bicicletas, Bici bici){
+    bicicletas.push_back(bici);
+}
+
+void viewList(list<Bici> bicicletas){
+    for (Bici bici : bicicletas) {
+        bici.verObjeto();
+	}
+}
+
+void countList(list<Bici> bicicletas){
+    std::cout << bicicletas.size() << '\n';
 }
